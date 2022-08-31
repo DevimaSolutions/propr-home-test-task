e892124075729e43e0986fc136acd6555db4e51d
taras kolomiiets <taras.k@devima.solutions>
2022-08-31 15:25:53 +0300

    add constants

---
+++ src/components/Stepper/components/OptionsStep/constants.ts
@@ -0,0 +1,8 @@
+import { IOption } from "./types";
+
+export const options: IOption[] = [
+  { text: "Right away", icon: "/images/clock.svg" },
+  { text: "1-3 Months", icon: "/images/new-calendar.svg" },
+  { text: "4+ Months", icon: "/images/calendar.svg" },
+  { text: "Already Listed", icon: "/images/schedule.svg" },
+];
--- src/components/Stepper/components/OptionsStep/index.tsx
+++ src/components/Stepper/components/OptionsStep/index.tsx
@@ -1,16 +1,13 @@
 import { Step } from "@/enums";
 import Image from "next/image";
+import { options } from "./constants";
 import styles from "./style.module.scss";
-import { IOption, IOptionsStepProps } from "./types";
+import { IOptionsStepProps } from "./types";
 
-const options: IOption[] = [
-  { text: "Right away", icon: "/images/clock.svg" },
-  { text: "1-3 Months", icon: "/images/new-calendar.svg" },
-  { text: "4+ Months", icon: "/images/calendar.svg" },
-  { text: "Already Listed", icon: "/images/schedule.svg" },
-];
-
-const OptionsStep = ({ step = Step.First, handleStepChange }: IOptionsStepProps) => {
+const OptionsStep = ({
+  step = Step.First,
+  handleStepChange,
+}: IOptionsStepProps) => {
   return (
     <div className={styles.root}>
       {options.map((option) => (
